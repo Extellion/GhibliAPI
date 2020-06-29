dbc11ace22a723bbd5ac458ef67382af6df3817d
extellion <allan@guichard.biz>
2020-06-29 17:36:44 +0200

    feat: add router and routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import logo from "./logo.svg";
 import "./App.css";
 import Movies from "./views/Movies";
+import Movie from "./views/Movie";
 
 function App() {
   return (
-    <div className="App">
-      <Movies />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Movies} />
+        <Route exact path="/movie/:id" render={(routeProps) => <Movie {...routeProps} />} />
+      </div>
+    </BrowserRouter>
   );
 }
 
