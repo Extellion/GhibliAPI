62610444156d933d70423b845a3d875256ea0146
extellion <allan@guichard.biz>
2020-06-29 17:03:06 +0200

    feat: plug redux to index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import reducers from "./redux/reducers/reducers";
+import { callAPIMiddleware } from "./redux/middleware/callAPI";
+import App from "./App";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+
+const initialState = {};
+
+const store = createStore(reducers, initialState, applyMiddleware(thunk, callAPIMiddleware));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
